edf0949168fb59f2a6159289a91a522416a41bff
Ekaterina Marchenko <kakomakako96@gmail.com>
2026-03-04 09:42:38 +0300

    fixup! Внесены правки согласно рекомендациям в ревью

--- src/test/java/ChromeQuestionsClickTest.java
+++ src/test/java/ChromeQuestionsClickTest.java
@@ -2,15 +2,12 @@ import org.example.QuestionsClick;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.stream.Stream;
 
 import static org.testng.AssertJUnit.assertTrue;
 
@@ -29,15 +26,9 @@ public class ChromeQuestionsClickTest {
         driver.get("https://qa-scooter.praktikum-services.ru/");
     }
 
-    private static Stream<Arguments> numberQuestions() {
-        return Stream.of(
-                Arguments.of(1 , 2, 3, 4, 5, 6, 7, 8),
-                Arguments.of(6, 1, 8, 3, 5, 2, 7, 4)
-        );
-    }
-
     @ParameterizedTest
-    @MethodSource("numberQuestions")
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 0})
+
     public void checkQuestions(int number) {
 
         //Создание объекта класса и запуск теста
